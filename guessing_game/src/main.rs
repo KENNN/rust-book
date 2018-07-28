b8fec29d45a0fa44c73b1998df02b9dbc9044645
KENN <cent.rngttwt@gmail.com>
2018-07-28 16:03:39 +0900

    add integer comparison

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,10 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
-fn main() {
+fn main(){
     println!("Gussing the number!");
 
 	let secret_number = rand::thread_rng().gen_range(1, 101);
@@ -16,5 +17,14 @@ fn main() {
 
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number");
+
     println!("Your guessed: {}", guess);
+
+    match guess.cmp(&secret_number){
+    	Ordering::Less => println!("Too small!"),
+    	Ordering::Greater => println!("Too big!"),
+    	Ordering::Equal => println!("You win!"),
+    }
 }
