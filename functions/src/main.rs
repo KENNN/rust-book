dfd8f9c13859d7354dd168b62135ab81f5b27187
KENN <cent.rngttwt@gmail.com>
2018-08-01 20:53:47 +0900

    add function with params

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
     println!("Hello, world!");
-    another_function();
+    another_function1();
+    another_function2(3);
+    another_function3(5, 6);
 }
 
-fn another_function() {
-	println!("Another Function.");
+fn another_function1() {
+	println!("Another function.");	
+}
+
+fn another_function2(x: i32) {
+	println!("x: {}", x);	
+}
+
+fn another_function3(x: i32, y: i32) {
+	println!("x: {}", x);	
+	println!("y: {}", y);	
+
 }
