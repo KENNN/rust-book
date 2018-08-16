e3c04f008950293a19070dbf7c16694f1b72eacd
KENN <cent.rngttwt@gmail.com>
2018-08-16 18:46:36 +0900

    add Using the Field Init Shorthand when Variables and Fields Have the Same Name

--- chapter5/defining_and_instantiating_structs/src/main.rs
+++ chapter5/defining_and_instantiating_structs/src/main.rs
@@ -1,25 +1,29 @@
 fn main() {
     let mut user1 = User {
-    	email: String::from("someone@example.com"),
-    	username: String::from("someoneusername123"),
-    	active: true,
-    	sign_in_count: 1,
+        email: String::from("someone@example.com"),
+        username: String::from("someoneusername123"),
+        active: true,
+        sign_in_count: 1,
     };
-	print!("{:?}", user1.email);
-	println!("{:?}", user1.username);	
-	println!("{:?}", user1.active);
-	println!("{:?}", user1.sign_in_count);	
+    print!("{:?}", user1.email);
+    println!("{:?}", user1.username);
+    println!("{:?}", user1.active);
+    println!("{:?}", user1.sign_in_count);
 
 
-	user1.email = String::from("another@example.com");
-	println!("{:?}", user1.email);
+    user1.email = String::from("another@example.com");
+    println!("{:?}", user1.email);
 
-	let user2 =  build_user(String::from("someone@example.com2"), String::from("someoneusername345"));
-	println!("{:?}", user2.username);
+    let user2 =  build_user(String::from("someone@example.com2"), String::from("someoneusername345"));
+    println!("{:?}", user2.username);
+
+    // using the field init shorthand when variables and fields have the same name
+    let user3 = build_user2(String::from("someone@example.com2"), String::from("someoneusername345"));
+    println!("{:?}", user3.email);
 }
 
 struct User {
-	username: String,
+    username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
@@ -27,10 +31,19 @@ struct User {
 
 
 fn build_user(email: String, username: String) -> User {
-	 User {
-    	email: email,
-    	username: username,
-    	active: true,
-    	sign_in_count: 1,
+     User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+fn build_user2(email:String, username:String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
     }
 }
