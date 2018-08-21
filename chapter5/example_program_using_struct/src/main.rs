3d1b9e26a325b596d08ba7d0890407ad8031384f
KENN <cent.rngttwt@gmail.com>
2018-08-21 21:40:21 +0900

    add Adding Useful Functionality with Derived Traits

--- chapter5/example_program_using_struct/src/main.rs
+++ chapter5/example_program_using_struct/src/main.rs
@@ -6,10 +6,15 @@ fn main() {
              area(width1, height1)
     );
 
+    // Refactoring with Tuples
     let rect1 = (30, 50);
     println!("The area of rectangle is {} square pixels.",
              area2(rect1)
     );
+
+    // Adding Useful Functionality with Derived Traits
+    let rect2 = Rectangle{width: 30, height: 50};
+    println!("rect2 is {:?}", rect2);
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -18,4 +23,10 @@ fn area(width: u32, height: u32) -> u32 {
 
 fn area2(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
