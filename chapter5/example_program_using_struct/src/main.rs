b15bf86e36590ce0288194491dfecd6240bdfea9
KENN <cent.rngttwt@gmail.com>
2018-08-21 21:36:01 +0900

    add Refactoring with Tuples

--- chapter5/example_program_using_struct/src/main.rs
+++ chapter5/example_program_using_struct/src/main.rs
@@ -5,8 +5,17 @@ fn main() {
     println!("The area of rectangle is {} square pixels.",
              area(width1, height1)
     );
+
+    let rect1 = (30, 50);
+    println!("The area of rectangle is {} square pixels.",
+             area2(rect1)
+    );
 }
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+fn area2(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
