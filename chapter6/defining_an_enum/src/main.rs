e6fe8c45c338ab2d23f063a9ada71b3df30fc188
KENN <cent.rngttwt@gmail.com>
2018-08-27 17:12:40 +0900

    [WIP] add Enum Values

--- chapter6/defining_an_enum/src/main.rs
+++ chapter6/defining_an_enum/src/main.rs
@@ -1,9 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+    // Enum Values
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    println!("{:?}", four);
+    println!("{:?}", six);
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("::1"),
+    };
+
+    println!("{:?}", home);
+    println!("{:?}", loopback);
 }
 
 #[derive(Debug)]
-enum IpAddrKing {
+enum IpAddrKind {
     V4,
     V6,
+}
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn route(ip_type: IpAddrKind) {
+    println!("{:?}", ip_type);
 }
