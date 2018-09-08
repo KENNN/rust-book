997beb56358feb7ae6b213b64a11df8f1e481e94
KENN <cent.rngttwt@gmail.com>
2018-09-08 21:55:16 +0900

    add Updating a Vector

--- chapter8/storing_lists_of_values_with_vectors/src/main.rs
+++ chapter8/storing_lists_of_values_with_vectors/src/main.rs
@@ -1,4 +1,13 @@
 fn main() {
     let v: Vec<i32> = Vec::new();
     let v = vec![1,2,3];
+    println!("{:?}", v);
+
+    // Updating a Vector
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+    println!("{:?}", v);
 }
