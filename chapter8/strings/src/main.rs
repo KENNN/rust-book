b896812aee00e4b782131d226113f51726ea35f5
KENN <cent.rngttwt@gmail.com>
2018-09-10 15:22:38 +0900

    add what is string

--- chapter8/strings/src/main.rs
+++ chapter8/strings/src/main.rs
@@ -1,8 +1,28 @@
 fn main() {
+    // what is string?
     // let mut s = String::new(); // new string
     let data = "initial contents";
     let s = data.to_string();
     // let s = "initial contents".to_string(); // equal to above
     // let s = String::from("initial contents"); // equal too
     println!("{:?}", s);
+
+    let hello = vec![
+    String::from("السلام عليكم"),
+    String::from("Dobrý den"),
+    String::from("Hello"),
+    String::from("שָׁלוֹם"),
+    String::from("नमस्ते"),
+    String::from("こんにちは"),
+    String::from("안녕하세요"),
+    String::from("你好"),
+    String::from("Olá"),
+    String::from("Здравствуйте"),
+    String::from("Hola"),
+    ];
+    for h in hello {
+        println!("{:?}", h);
+    }
+
+
 }
