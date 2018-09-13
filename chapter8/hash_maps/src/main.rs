b53485f3a2227863ed7f3918d454b597c618c9aa
KENN <cent.rngttwt@gmail.com>
2018-09-13 13:46:03 +0900

    add hash maps

---
+++ chapter8/hash_maps/src/main.rs
@@ -0,0 +1,13 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    println!("{:?}", scores);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let scores: HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("{:?}", scores);
+}
