a3f028eb3ae86c4239c95e0fdfa15644d67c1489
KENN <cent.rngttwt@gmail.com>
2018-09-14 21:26:20 +0900

    add ccessing Value in a Hash Map

--- chapter8/hash_maps/src/main.rs
+++ chapter8/hash_maps/src/main.rs
@@ -17,4 +17,14 @@ fn main() {
     let mut map = HashMap::new();
     map.insert(field_name, field_value);
     println!("{:?}", map);
+
+    // Accessing Value in a Hash Map
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+    println!("{:?}", score);
+
 }
