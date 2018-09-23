7d3b3d7d80eb15e738d099fb85f7939304d3a0c8
KENN <cent.rngttwt@gmail.com>
2018-09-23 11:46:22 +0900

    add To panic! or Not to panic

--- chapter9/to_panic_or_not_to_panic/src/main.rs
+++ chapter9/to_panic_or_not_to_panic/src/main.rs
@@ -4,4 +4,15 @@ fn main() {
     // We know 127.0.0.1 is valid IP Addr but a conpiler don't.
     // The compiler requires use of Result type.
     let home: IpAddr = "127.0.0.1".parse().unwrap();
+
+    let guess = String::from("111");
+    let guess: i32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("not i32")
+    };
+
+    if guess < 1 || guess > 100 {
+        println!("The secret number will be between 1 and 100.");
+        return;
+    }
 }
