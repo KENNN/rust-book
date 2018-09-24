0235fc8c4c8b6a4a1f53cb2ede1ab97fcfa06619
KENN <cent.rngttwt@gmail.com>
2018-09-24 14:14:33 +0900

    add  Guidelines for Error Handling

--- chapter9/to_panic_or_not_to_panic/src/main.rs
+++ chapter9/to_panic_or_not_to_panic/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     // The compiler requires use of Result type.
     let home: IpAddr = "127.0.0.1".parse().unwrap();
 
+    // Guidelines for Error Handling
     let guess = String::from("111");
     let guess: i32 = match guess.trim().parse() {
         Ok(num) => num,
@@ -13,6 +14,29 @@ fn main() {
 
     if guess < 1 || guess > 100 {
         println!("The secret number will be between 1 and 100.");
-        return;
+    }
+
+    let g = Guess::new(111);
+    println!("{:?}", g);
+}
+
+#[derive(Debug)]
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
     }
 }
