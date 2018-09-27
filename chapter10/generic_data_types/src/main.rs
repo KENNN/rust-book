544363cf044cb8ee5d03b3e0d20c294676995fcd
KENN <cent.rngttwt@gmail.com>
2018-09-27 23:31:44 +0900

    add Generic Data Types

--- chapter10/generic_data_types/src/main.rs
+++ chapter10/generic_data_types/src/main.rs
@@ -8,6 +8,12 @@ fn main() {
 
     let result = largest_char(&char_list);
     println!("The largest char is {:?}", result);
+
+    let result = largest(&number_list);
+    println!("The largest number is {:?}", result);
+
+    let result = largest(&char_list);
+    println!("The largest char is {:?}", result);
 }
 
 fn largest_i32(list: &[i32]) -> i32 {
@@ -24,6 +30,16 @@ fn largest_i32(list: &[i32]) -> i32 {
 fn largest_char(list: &[char]) -> char {
     let mut largest = list[0];
 
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
     for &item in list.iter() {
         if item > largest {
             largest = item;
