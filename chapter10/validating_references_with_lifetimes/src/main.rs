02d7914e34450870be29431df8dd7b55c0ca4dd6
KENN <cent.rngttwt@gmail.com>
2018-10-08 17:38:27 +0900

    add Lifetime Annotation in Struct Definition

--- chapter10/validating_references_with_lifetimes/src/main.rs
+++ chapter10/validating_references_with_lifetimes/src/main.rs
@@ -43,6 +43,15 @@ fn main() {
     }
     println!("The longest string is {}", result);
     */
+
+    // Lifetime Annotation in Struct Definition
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+
+    let i = ImportantExcerpt { part: first_sentence };
+    println!("{:?}", i);
 }
 
 fn longest<'a> (x: &'a str, y: &'a str) -> &'a str {
@@ -64,3 +73,8 @@ fn longest3<'a> (x: &str, y:&str) -> 'a str {
     result.as_str()
 }
 */
+
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
