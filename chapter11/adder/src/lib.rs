4f066b2a3387be58a32c921d22980c316075b2a4
KENN <cent.rngttwt@gmail.com>
2018-10-17 11:47:57 +0900

    add Checking for Panic with should_panic

--- chapter11/adder/src/lib.rs
+++ chapter11/adder/src/lib.rs
@@ -56,6 +56,13 @@ mod tests {
                 "Greeting did not contain name, value was `{}`", result
         );
     }
+
+    // Checking for Panic with should_panic
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
 }
 
 #[derive(Debug)]
@@ -81,3 +88,21 @@ pub fn add_two(a: i32) -> i32 {
 pub fn greeting(name: &str) -> String {
     String::from("Hello!")
 }
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1  {
+            panic!("Guess value must be less than or equal to 100, got {}.", value);
+        } else if value > 100 {
+            panic!("Guess value must be greater than or equal to 1, got {}.", value);
+        }
+        Guess {
+            value
+        }
+    }
+}
+
