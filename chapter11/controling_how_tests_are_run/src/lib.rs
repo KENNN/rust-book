c74e7a44764b1cc82e564d983d40cded7c510ed9
KENN <cent.rngttwt@gmail.com>
2018-10-19 22:54:35 +0900

    add Running a Subset of Tests by Name

--- chapter11/controling_how_tests_are_run/src/lib.rs
+++ chapter11/controling_how_tests_are_run/src/lib.rs
@@ -13,9 +13,29 @@ mod tests {
         let value = prints_and_returns_10(8);
         assert_eq!(5, value);
     }
+
+    // Running a Subset of Tests by Name
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn add_one_hundred_and_two() {
+        assert_eq!(102, add_two(100));
+    }
 }
 
 fn prints_and_returns_10(a: i32) -> i32 {
     println!("I got the value {}", a);
     10
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
