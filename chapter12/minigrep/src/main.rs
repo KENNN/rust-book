6c4138aa38d9c360cfac2814746e9cf927e0b46e
KENN <cent.rngttwt@gmail.com>
2018-10-26 16:33:37 +0900

    add Creating a Constructor for Config

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -25,6 +25,14 @@ struct Config {
     filename: String,
 }
 
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
+}
 
 fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
