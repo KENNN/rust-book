e1a16690a4d64be4ba25f93d435fcc4a26fb8d89
KENN <cent.rngttwt@gmail.com>
2018-10-29 11:06:55 +0900

    add Returning Errors from the run Function

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -44,11 +45,11 @@ fn parse_config(args: &[String]) -> Config {
     Config { query, filename }
 }
 
-fn run(config: Config) {
+fn run(config: Config) -> Result<(), Box<Error>> {
     let mut f = File::open(config.filename).expect("file not found");
     let mut contents = String::new();
 
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
     println!("With text: \n{}", contents);
+    Ok(())
 }
