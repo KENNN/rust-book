0872f15779b126b1cc00825e27a44bfc31a4b4ac
KENN <cent.rngttwt@gmail.com>
2018-10-30 21:35:26 +0900

    add Splitting Code into a Library Crate

--- chapter12/minigrep/src/main.rs
+++ chapter12/minigrep/src/main.rs
@@ -1,8 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
+use minigrep::Config;
+use minigrep::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,39 +21,4 @@ fn main() {
         println!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-#[derive(Debug)]
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename).expect("file not found");
-    let mut contents = String::new();
-
-    f.read_to_string(&mut contents)?;
-    println!("With text: \n{}", contents);
-    Ok(())
 }
