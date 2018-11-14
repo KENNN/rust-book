429704969446ced164e6dcc3b1cc17949a4cf650
KENN <cent.rngttwt@gmail.com>
2018-11-14 15:26:55 +0900

    add Refactering Using Functions

--- chapter13/closures_anonymous_functions_that_can_capture_their_environment/src/main.rs
+++ chapter13/closures_anonymous_functions_that_can_capture_their_environment/src/main.rs
@@ -12,20 +12,26 @@ fn main() {
 }
 
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculattion slowly...");
+    println!("calculation slowly...");
     thread::sleep(Duration::from_secs(2));
     intensity
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-             simulated_expensive_calculation(intensity)
+             expensive_closure(intensity)
         );
         println!(
             "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
+            expensive_closure(intensity)
         );
     } else {
         if random_number == 3 {
@@ -35,7 +41,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
+                expensive_closure(intensity)
             );
         }
     }
