ddab5d1c5ef1a009323b1cdfd92ae5e250919185
KENN <cent.rngttwt@gmail.com>
2018-11-22 22:27:39 +0900

    add Capturing the Environment with Closures

--- chapter13/closures_anonymous_functions_that_can_capture_their_environment/src/main.rs
+++ chapter13/closures_anonymous_functions_that_can_capture_their_environment/src/main.rs
@@ -14,7 +14,27 @@ fn main() {
     let example_closure = |x| x;
     let s = example_closure(String::from("hello"));
     let n = example_closure(5);
-    */
+*/
+
+    let x = 4;
+    let equal_to_x = |z| z == x;
+    let y = 4;
+
+    assert!(equal_to_x(y));
+
+/* error
+    let x = 4;
+    fn equal_to_x(z: i32) -> bool { z == x }
+    let y = 4;
+
+    assert!(equal_to_x(y));
+*/
+
+    let x = vec![1, 2, 3];
+    let equal_to_x = move |z| z == x;
+    println!("can't use x here: {:?}", x);
+    let y = vec![1, 2, 3];
+    assert!(equal_to_x(y));
 }
 
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
