f6b0db886c02c5c759b489a685d0213156f68474
KENN <cent.rngttwt@gmail.com>
2018-12-09 21:52:43 +0900

    Using Our Counter Iterator's next Method

--- chapter13/processing_a_series_of_items_with_iterators/src/lib.rs
+++ chapter13/processing_a_series_of_items_with_iterators/src/lib.rs
@@ -38,6 +38,18 @@ mod tests {
         );
     }
 
+    #[test]
+    fn calling_next_directly() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
+
     #[derive(PartialEq, Debug)]
     struct Shoe {
         size: u32,
@@ -56,7 +68,7 @@ mod tests {
 
     impl Counter {
         fn new() -> Counter {
-            Counter {count: 0}
+            Counter { count: 0 }
         }
     }
 
