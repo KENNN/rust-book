72c093f30ec56b487df6a741023b4faeaec5b60c
KENN <cent.rngttwt@gmail.com>
2018-12-18 21:43:15 +0900

    add Treating Type Like a Reference by Implementing the Deref Trait

--- chapter15/treating_smart_pointers_like_regular_references_with_the_deref_trait/src/main.rs
+++ chapter15/treating_smart_pointers_like_regular_references_with_the_deref_trait/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 fn main() {
     let x = 5;
     let y = &x;
@@ -10,4 +12,27 @@ fn main() {
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+#[derive(Debug)]
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
 }
