5f06d7a5c769f0fc62fc25897be476708e434ee0
KENN <cent.rngttwt@gmail.com>
2018-12-20 17:52:54 +0900

    Running Code On Cleanup With the Drop Trait

---
+++ chapter15/running_code_on_cleanup_with_the_drop_trait/src/main.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}` !", self.data);
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer { data: String::from("my staff")};
+    let d = CustomSmartPointer { data: String::from("other staff")};
+    println!("CustomSmartPointers created.");
+}
