0f90b600ada5de85d36a578773e3e35aa520ca2d
KENN <cent.rngttwt@gmail.com>
2018-12-26 19:19:41 +0900

    [WIP] add Enforcing Borrowing Reules at Runtime with RefCell<T>

--- chapter15/recallt_and_the_interior_mutability_pattern/src/lib.rs
+++ chapter15/recallt_and_the_interior_mutability_pattern/src/lib.rs
@@ -32,3 +32,26 @@ impl<'a, T> LimitTracker<'a, T>
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockMessenger {
+        sent_messages: Vec<String>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger { sent_messages: vec![] }
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+        limit_tracker.set_value(80);
+        assert_eq!(mock_messenger.sent_messages.len(), 1);
+    }
+}
