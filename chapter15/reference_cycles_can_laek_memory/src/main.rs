20fd9ce69a2fa07b3b1eb0728d1cf3daa341cb8f
KENN <cent.rngttwt@gmail.com>
2019-01-01 11:59:50 +0900

    add Adding a Reference from a Child to Its Parent

--- chapter15/reference_cycles_can_laek_memory/src/main.rs
+++ chapter15/reference_cycles_can_laek_memory/src/main.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 use List::{Cons, Nil};
 
@@ -20,6 +20,7 @@ impl List {
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
@@ -46,12 +47,19 @@ fn main() {
 
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
 
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
