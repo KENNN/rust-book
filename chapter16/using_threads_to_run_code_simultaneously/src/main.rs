5b7c2ce37193e82068b1e05469197eb5ddf11b4d
KENN <cent.rngttwt@gmail.com>
2019-01-06 22:28:33 +0900

    add Using move Closures with Threads

--- chapter16/using_threads_to_run_code_simultaneously/src/main.rs
+++ chapter16/using_threads_to_run_code_simultaneously/src/main.rs
@@ -16,5 +16,12 @@ fn main() {
         thread::sleep(Duration::from_millis(1));
     }
 
-//    handle.join().unwrap();
+    // handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
 }
