bb543945f300863f0525c6c591536327d84647e4
KENN <cent.rngttwt@gmail.com>
2019-01-18 23:20:27 +0900

    [WIP] add Imprementing the Trait

---
+++ chapter17/using_trait_objects_that_allow_for_values_of_different_types/src/main.rs
@@ -0,0 +1,41 @@
+extern crate gui;
+use gui::Draw;
+use gui::{Screen, Button};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+     }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    // はい
+                    String::from("Yes"),
+                    // 多分
+                    String::from("Maybe"),
+                    // いいえ
+                    String::from("No")
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                // 了解
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
