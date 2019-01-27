361024dd012e7301a9fe986b89b69dcbf832b36b
KENN <cent.rngttwt@gmail.com>
2019-01-27 21:35:45 +0900

    add Trade-offs of the State Pattern

--- chapter17/imprementing_an_object-oriented_design_pattern/src/lib.rs
+++ chapter17/imprementing_an_object-oriented_design_pattern/src/lib.rs
@@ -1,5 +1,4 @@
-use std::boxed::Box;
-
+/*
 pub struct Post {
     state: Option<Box<State>>,
     content: String,
@@ -79,5 +78,51 @@ impl State for Published {
 
     fn content<'a>(&self, post: &'a Post) -> &'a str {
         &post.content
+    }
+}
+
+*/
+
+pub struct Post {
+    content: String,
+}
+
+pub struct DraftPost {
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> DraftPost {
+        DraftPost {
+            content: String::new(),
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPost {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
+        }
+    }
+}
+
+pub struct PendingReviewPost {
+    content: String,
+}
+
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
     }
 }
