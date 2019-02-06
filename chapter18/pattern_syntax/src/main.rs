20d6d0d2813416089fa2c37d39d00e462d17f88c
KENN <cent.rngttwt@gmail.com>
2019-02-06 22:56:15 +0900

    add Destructuring Structs

--- chapter18/pattern_syntax/src/main.rs
+++ chapter18/pattern_syntax/src/main.rs
@@ -41,4 +41,22 @@ fn main() {
         'k' ... 'z' => println!("late ASCII letter"),
         _ => println!("something else"),
     }
+
+    let p = Point { x: 0, y: 7 };
+    let Point { x: a, y: b } = p;
+
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
 }
+
+#[derive(Debug)]
+    struct Point {
+        x: i32,
+        y: i32,
+    }
