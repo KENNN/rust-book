5c5877449e59b2d1302b17cdaa7a2cbcae5d28df
KENN <cent.rngttwt@gmail.com>
2019-02-07 21:56:50 +0900

    add Destructuring Enums

--- chapter18/pattern_syntax/src/main.rs
+++ chapter18/pattern_syntax/src/main.rs
@@ -53,6 +53,23 @@ fn main() {
         Point { x: 0, y } => println!("On the y axis at {}", y),
         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
     }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destrycture");
+        },
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y);
+        },
+        Message::Write(text) => {
+            println!("Text message: {}", text);
+        },
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b);
+        },
+    }
+
 }
 
 #[derive(Debug)]
@@ -60,3 +77,12 @@ fn main() {
         x: i32,
         y: i32,
     }
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
