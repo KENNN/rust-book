7ad72a2d23e28588d2fba59f38c9cf83d5345c61
KENN <cent.rngttwt@gmail.com>
2019-02-08 22:24:31 +0900

    add Destructuring References

--- chapter18/pattern_syntax/src/main.rs
+++ chapter18/pattern_syntax/src/main.rs
@@ -70,6 +70,18 @@ fn main() {
         },
     }
 
+    let points = vec![
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 5 },
+        Point { x: 10, y: -3 },
+    ];
+
+    let sum_of_squares: i32 = points
+        .iter()
+        .map(|&Point { x, y } | x * x + y * y)
+        .sum();
+
+    println!("{:?}", sum_of_squares);
 }
 
 #[derive(Debug)]
