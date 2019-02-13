ced8b3c79dcf613bcea99fc8ba49b7b34f0e33dd
KENN <cent.rngttwt@gmail.com>
2019-02-13 23:19:52 +0900

    add Ignoring Remaining Parts of a Value with ..

--- chapter18/pattern_syntax/src/main.rs
+++ chapter18/pattern_syntax/src/main.rs
@@ -116,6 +116,17 @@ fn main() {
         println!("found a string");
     }
     println!("{:?}", s);
+
+    let origin = Point2 { x: 0, y: 0, z: 0 };
+    match origin {
+        Point2 { x, .. } => println!("x is {}", x),
+    }
+
+    let numbers = (2, 4, 8, 16, 32);
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last)},
+    }
 }
 
 #[derive(Debug)]
@@ -136,3 +147,9 @@ fn foo(_: i32, y:i32) {
     println!("This code only uses the y parameter: {}", y);
 }
 
+#[derive(Debug)]
+struct Point2 {
+    x: i32,
+    y: i32,
+    z: i32,
+}
