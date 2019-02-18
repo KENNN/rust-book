5cb49ed2d1bfd98cb3d76c44655b90f29c243f83
KENN <cent.rngttwt@gmail.com>
2019-02-18 22:32:30 +0900

    add Calling an Unsafe Function or Method

--- chapter19/unsafe_rust/src/main.rs
+++ chapter19/unsafe_rust/src/main.rs
@@ -5,10 +5,19 @@ fn main() {
     let r2 = &mut num as &mut i32;
 
     let address = 0x012345usize;
-    let r = address as *const i32;
+    let _r = address as *const i32;
 
     unsafe {
         println!("r1 is: {}", *r1);
         println!("r2 is: {}", *r2);
+    }
+
+    unsafe fn dangerous() {}
+
+    // error
+    // dangerous();
+
+    unsafe {
+        dangerous();
     }
 }
