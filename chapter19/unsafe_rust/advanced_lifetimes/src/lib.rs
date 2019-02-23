acd8ba5ee4bbdcc8eb506d0758059f2a21195cf4
KENN <cent.rngttwt@gmail.com>
2019-02-23 22:42:28 +0900

    add Advanced Lifetimes

---
+++ chapter19/unsafe_rust/advanced_lifetimes/src/lib.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+struct Context(&str);
+
+#[derive(Debug)]
+struct Parser {
+    context: &Context,
+}
+
+impl Parser {
+    fn parse(&self) -> Result<(), &str> {
+        Err(&self.context.0[1..])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
