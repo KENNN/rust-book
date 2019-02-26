79c62d40c49592de82aea7a06f7de790b1f6a930
KENN <cent.rngttwt@gmail.com>
2019-02-26 23:18:21 +0900

    add Specifying Placeholder Types in Trait Definitions with Associated Types

---
+++ chapter19/advanced_traits/src/lib.rs
@@ -0,0 +1,21 @@
+pub trait Iterator {
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
