7c1d6e0afc6de83e765aa82c4c0c9d063bed5f68
KENN <cent.rngttwt@gmail.com>
2019-03-01 23:11:13 +0900

    [WIP] add Fully Qualified Syntax for Disambiguation: Calling Methods with the Same Name

--- chapter19/advanced_traits/src/main.rs
+++ chapter19/advanced_traits/src/main.rs
@@ -17,7 +17,38 @@ impl Add for Point {
     }
 }
 
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+    println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
 fn main() {
     assert_eq!(Point{ x: 1, y: 0 } + Point{ x:2, y: 3 },
                Point{ x: 3 , y:3 });
+
+    let person = Human;
+    person.fly();
 }
