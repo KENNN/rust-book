2058a0333c0810e23b04a0c65a5eb1a05d231827
KENN <cent.rngttwt@gmail.com>
2019-03-06 23:30:52 +0900

    add The Never Type that Never Returns

--- chapter19/advanced_types/src/main.rs
+++ chapter19/advanced_types/src/main.rs
@@ -4,4 +4,19 @@ fn main() {
     let x: i32 = 5;
     let y: Kilometers = 5;
     println!("x + y = {}", x + y);
+
+    let guess = "5";
+    loop {
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    }
+
+    print!("forever ");
+
+    loop {
+        print!("and ever ");
+        break
+    }
 }
