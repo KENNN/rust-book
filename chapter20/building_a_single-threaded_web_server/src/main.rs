16cf0e2dae255a0c7f63bccdd5f12ca9c582dcbe
KENN <cent.rngttwt@gmail.com>
2019-03-11 22:53:57 +0900

    add Returning Real HTML

--- chapter20/building_a_single-threaded_web_server/src/main.rs
+++ chapter20/building_a_single-threaded_web_server/src/main.rs
@@ -1,11 +1,21 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::fs::File;
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let mut file = File::open("hello.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
 
 fn main() {
@@ -16,5 +26,3 @@ fn main() {
         handle_connection(stream);
     }
 }
-
-
